6cceccbb5ebd55d48de92fd0209b251f2c0bcac6
Nyiro Gergo <gergo.nyiro@gmail.com>
2014-10-25 14:33:22 +0200

    linkreader

---
+++ src/feedlink/linkreader.py
@@ -0,0 +1,32 @@
+from HTMLParser import HTMLParser
+
+class LinkReader(HTMLParser):
+    _link_tags = {'link', 'a'}
+    _link_attr = 'href'
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.links = []
+        return
+
+    def handle_starttag(self, tag, attrs):
+        if tag not in self._link_tags:
+            return
+        attrs = dict(attrs)
+        if self._link_attr in attrs:
+            self.links.append(attrs[self._link_attr])
+        return
+
+def get_links(fh):
+    """
+    Read the href attributes from link and a tag of fh html file
+    
+    Args:
+        fh: file handle of the html file
+
+    Returns:
+        list of the links
+    """
+    html = fh.read()
+    reader = LinkReader()
+    reader.feed(html)
+    return reader.links
---
+++ test/feedlink/linkreader.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+
+from unittest import TestCase, main
+from os.path import dirname, realpath
+
+basedir = realpath(dirname(__file__)+'/../..')
+sys.path.append(basedir + '/src')
+from feedlink.linkreader import LinkReader, get_links
+
+class TestLinkReader(TestCase):
+    def setUp(self):
+        self.reader = LinkReader()
+        return
+
+    def test_LinkReader_feeding(self):
+        html = '''
+        <a href="foo/bar">
+        <link href="spam/egg">
+        '''
+        self.reader.feed(html)
+        self.assertListEqual(self.reader.links, ['foo/bar', 'spam/egg'])
+        return
+
+    def test_get_links(self):
+        with open(basedir+'/test/data/feeds.html') as fh:
+            links = get_links(fh)
+        return
+
+if __name__ == '__main__':
+    main()
+
