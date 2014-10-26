be400a0b6a13894717babbb4384cff9e0b31966d
Nyiro Gergo <gergo.nyiro@gmail.com>
2014-10-26 13:44:16 +0100

    classfeedlinks setup timeout for urlopen

--- test/test_bin_classfeedlinks.py
+++ test/test_bin_classfeedlinks.py
@@ -21,16 +21,19 @@ class TestGetOptions(TestCase):
     def test_get_options(self):
         options = feedlinks.get_options([
             basedir+'/test/data/feeds.html',
-            '--json', basedir+'/test/data/feeds.json'
+            '--json', basedir+'/test/data/feeds.json',
+            '--timeout', '45',
         ])
         self.assertIsInstance(options['html'], file)
         self.assertIsInstance(options['json'], file)
+        self.assertEqual(options['timeout'], 45)
         return
 
     def test_get_options_defaults(self):
         options = feedlinks.get_options([])
         self.assertIsInstance(options['html'], file)
         self.assertIsInstance(options['json'], file)
+        self.assertEqual(options['timeout'], 60)
         return
 
 class TestMain(TestCase):
@@ -46,7 +49,7 @@ class TestMain(TestCase):
 
     def test_main(self):
         stdout = StringIO()
-        feedlinks.main(self.stdin, stdout)
+        feedlinks.main(self.stdin, stdout, 60)
         stdout.seek(0)
         feeds = load(stdout)
         self.assertDictEqual(feeds,
@@ -72,7 +75,7 @@ class TestBuild(TestCase):
         return
 
     def test(self):
-        feeds = feedlinks.build(self.links)
+        feeds = feedlinks.build(self.links, 60)
         self.assertDictEqual(feeds,
             {
                 'rss': [
